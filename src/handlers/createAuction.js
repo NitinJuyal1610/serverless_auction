d1bddf118f9d38e8e39111e182a4d08c2c27d22d
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2023-10-30 17:42:39 +0530

    dynamoDb write yml

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,8 +1,37 @@
-async function createAuction(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+'use strict';
+import crypto from 'crypto';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+
+const client = new DynamoDBClient({ region: 'ap-south-1' });
+const docClient = DynamoDBDocumentClient.from(client);
+
+const createAuction = async (event, context) => {
+  const body = JSON.parse(event.body);
+  const auction = {
+    id: crypto.randomUUID(),
+    title: body.title,
+    status: 'OPEN',
+    createdAt: new Date().toISOString(),
   };
-}
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction,
+  };
+  let result = {};
+  let status = 201;
+  try {
+    result = await docClient.send(new PutCommand(params));
+    console.log(`result: ${JSON.stringify(result, null, 2)}`);
+  } catch (error) {
+    status = 500;
+    console.log('error:', error);
+  }
+
+  return {
+    statusCode: status,
+    body: JSON.stringify(result),
+  };
+};
 
 export const handler = createAuction;
