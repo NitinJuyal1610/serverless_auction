9b37f13535d831a9b374ecd64ad6bed8ac28dc49
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2023-11-05 20:43:31 +0530

    validation base64 image

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -3,17 +3,39 @@ import { uploadPictureToS3 } from '../../lib/uploadPictureToS3.js';
 import middy from '@middy/core';
 import httpErrorHandler from '@middy/http-error-handler';
 import createHttpError from 'http-errors';
+import { addPictureUrl } from '../../lib/addPictureUrl.js';
+import validator from '@middy/validator';
+import { transpileSchema } from '@middy/validator/transpile';
+import addPictureSchema from '../../lib/schemas/addPictureSchema.js';
 async function uploadAuctionPicture(event, context) {
   const { id } = event.pathParameters;
   const auction = await getAuctionById(id);
+
+  const { email } = event.requestContext.authorizer;
+
+  if (auction.seller !== email) {
+    throw new createHttpError.Forbidden(
+      'You are not the seller of this auction',
+    );
+  }
+
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64, 'base64');
 
+  if (buffer.toString('base64') !== base64) {
+    throw new createHttpError.BadRequest(
+      'An invalid base64 string was provided for the auction image.',
+    );
+  }
+
   const key = `${auction.id}.jpeg`;
+  let updatedResult;
   try {
     await uploadPictureToS3(key, buffer);
 
     const pictureUrl = `https://${process.env.AUCTIONS_BUCKET_NAME}.s3.amazonaws.com/${key}`;
+    updatedResult = await addPictureUrl(auction, pictureUrl);
+    console.log(updatedResult);
   } catch (error) {
     console.error(error);
     throw new createHttpError.InternalServerError(error);
@@ -21,8 +43,18 @@ async function uploadAuctionPicture(event, context) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedResult),
   };
 }
 
-export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler())
+  .use(
+    validator({
+      eventSchema: transpileSchema(addPictureSchema),
+      i18nEnabled: false,
+      ajvOptions: {
+        useDefaults: true,
+      },
+    }),
+  );
